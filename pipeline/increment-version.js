67854e87386e20a071babaca7694641991a7dc70
Jacob Darker <jacob@darker.software>
2024-04-04 23:48:01 +0100

    change from require to import

--- pipeline/increment-version.js
+++ pipeline/increment-version.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "node:fs";
+import path from "node:path";
 
 const manifestPath = path.join(__dirname, "manifest.json");
 const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
